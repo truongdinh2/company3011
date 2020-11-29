74a1875736746f35a809b7bb459534635830daa4
truongdinh2 <dinhtruong2808u@gmail.com>
2020-11-29 23:52:10 +0700

    first commit1

--- src/component/management/Dialog.jsx
+++ src/component/management/Dialog.jsx
@@ -8,13 +8,13 @@ const layout = {
 };
 
 const validateMessages = {
-  required: `${label} is required`,
+  required: '${label} is required!',
   types: {
-    email: `${label} is not a valid email!`,
-    number: `${label} is not a valid number!`,
+    email: '${label} is not a valid email!',
+    number: '${label} is not a valid number!',
   },
   number: {
-    range: `${label} must be between ${min} and ${max}`,
+    range: '${label} must be between ${min} and ${max}',
   },
 };
 
